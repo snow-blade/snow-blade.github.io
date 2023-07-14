cdc470a46372f06424a5a9de38b48f3380430e74
noryve-03 <freeman.irabaruta@yale.edu>
2023-07-13 22:32:53 -0400

    One of the first commits, I guess

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,7 +6,7 @@
 
 const url = 'https://api.openweathermap.org/data/2.5/'
 const key = '739ef515e3ade2c79525a82f51f458a5'
-const cityName = 'Istanbul'
+const cityName = 'New Haven'
 
 $.getJSON(`${url}weather?q=${cityName}&appid=${key}&units=metric&lang=en`, function(data) {
     $("#weather").html(`<span class="text-capitalize">${data.weather[0].description}</span> of ${Math.round(data.main.temp)}°C in ${data.name}, ${data.sys.country}`);
